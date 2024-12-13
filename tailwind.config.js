5b544622b75a3cf5ef31b64c6f4667b9c7c70e4c
sabina4008 <sabinachauhan4008@gmail.com>
2024-12-13 09:13:45 -0800

    Update Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       fontFamily:{
         Figtree:['Figtree', "sans-serif"],
+        FigtreeExtraBold: ['FigtreeExtraBold', "sans-serif"],
       },
       colors: {
         'custom-yellow': '#F4D04E',
@@ -12,12 +13,21 @@ module.exports = {
       width: {
         '89': '89%',
         '90': '365px',
+        '82': '82px',
+        '129px': '129px',
+      },
+      height:{
+        '522': '522px',
+        '29':'29px',
       },
       boxShadow: {
         'solid-shad':'8px 7px black',
       },
       fontSize:{
         '15':'15px',
+      }, 
+      borderRadius: {
+        '10': '10px',
       }
     },
   },
